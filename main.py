bff6cfd768261e643949411106169b1886c21ae1
Apurva Joshi <zapurva@gmail.com>
2021-05-06 09:14:32 +0530

    added a few sanity checks and exceptions for robustness

--- main.py
+++ main.py
@@ -29,49 +29,70 @@ def hdl_time(week_no):
     return chk_date
 
 def hdl_request(place, week_no):
-    preamble = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id='
+    preamble = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id='
     link = preamble + place + '&date=' + hdl_time(week_no)
-    response = requests.get(link, timeout = 1)
-    #response = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=395&date=02-05-2021')
-    print('-------------------------------------------')
-    print('Checking for week starting: ', hdl_time(week_no))
-    print('-------------------------------------------')
-    data = response.json()
+    #print(link)
+    headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0'}
+    try:
+        response = requests.get(link, headers = headers, timeout = 3)
+        #print(response)
+    except requests.exceptions.SSLError:
+        beepy.beep(sound='error')
+        print('[ERROR] SSL Certificate issue')
 
-    no_of_centers = len(data["centers"])
-    print("Total no. of centers providing vaccinations: ", no_of_centers)
-
-    ref_id = []
-    ref_id_age = []
-
-    for i in range(0,no_of_centers):
+    if response.status_code == 200:
+    
+    #if response.raise_for_status() is not None:
+    #   beepy.beep(sound=1)
+    #print(response.raise_for_status())
+    
+        print('-------------------------------------------')
+        print('Checking for week starting: ', hdl_time(week_no))
+        print('-------------------------------------------')
+        data = response.json()
+        #print(data)
         
-        sample = data["centers"][i]
-        sessions_data = sample["sessions"]
-        sessions_data = sessions_data[0]
-        available = sessions_data["available_capacity"]
-        if available > 0:
-            ref_id.append(i)
+        try:
+            no_of_centers = len(data["centers"])
+            print("Total no. of centers providing vaccinations: ", no_of_centers)
 
-    no_vacc_centers = len(ref_id)
-    print('No. of centers with open slots: ', no_vacc_centers)
-    # data_filtered = {k: data["centers"][k] for k in ref_id}
-    # print('Details of centers: ', data_filtered)
+            ref_id = []
+            ref_id_age = []
 
-    # Check whether slots are open for 18+
-    for i in ref_id:
-        age = data["centers"][i]["sessions"][0]["min_age_limit"]
-        if age == 18:
-            ref_id_age.append(i)
-    no_vacc_centers_18yo = len(ref_id_age)
-    print('No. of centers with open slots for age 18+: ', no_vacc_centers_18yo)
-    if no_vacc_centers_18yo > 0:
-        for i in ref_id_age:
-            pincode = data["centers"][i]["pincode"]
-            if pincode < 400800:
-                #audio_alert()
-                beepy.beep(sound=6)
-                print(data["centers"][i])
+            for i in range(0,no_of_centers):
+                
+                sample = data["centers"][i]
+                sessions_data = sample["sessions"]
+                sessions_data = sessions_data[0]
+                available = sessions_data["available_capacity"]
+                if available > 0:
+                    ref_id.append(i)
+
+            no_vacc_centers = len(ref_id)
+            print('No. of centers with open slots: ', no_vacc_centers)
+            # data_filtered = {k: data["centers"][k] for k in ref_id}
+            # print('Details of centers: ', data_filtered)
+
+            # Check whether slots are open for 18+
+            for i in ref_id:
+                age = data["centers"][i]["sessions"][0]["min_age_limit"]
+                if age == 18:
+                    ref_id_age.append(i)
+            no_vacc_centers_18yo = len(ref_id_age)
+            print('No. of centers with open slots for age 18+: ', no_vacc_centers_18yo)
+            if no_vacc_centers_18yo > 0:
+                for i in ref_id_age:
+                    pincode = data["centers"][i]["pincode"]
+                    if pincode < 400800:
+                        #audio_alert()
+                        beepy.beep(sound=6)
+                        print(data["centers"][i])
+            
+        except KeyError:
+            beepy.beep(sound='error')
+
+    else:
+        print("[ERROR] Bad response from server", response)        
 
 if __name__ == "__main__":
     no_of_weeks = 3 
@@ -82,7 +103,7 @@ if __name__ == "__main__":
         for week in range(0, no_of_weeks):
             hdl_request(TNA, week)
         
-        time.sleep(20)
+        time.sleep(15)
         
         print('***********************************************')
         print('Checking for free slots in Mumbai at', datetime.datetime.now())
@@ -90,4 +111,4 @@ if __name__ == "__main__":
         for week in range(0, no_of_weeks):
             hdl_request(MUM, week)
 
-        time.sleep(20)
+        time.sleep(15)
